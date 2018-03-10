02d5717c5e4c81e731be63073b7392d5be710e6b
sivakirantamma <tammasivakiran@gmail.com>
2018-03-10 17:07:27 +0530

    new version 3rd day of workshop

--- main.js
+++ main.js
@@ -1,1 +1,59 @@
-document.getElementsById('data')
+
+function loadJSON(file,callback) {
+  var rawFile= new XMLHttpRequest();
+  rawFile.overrideMimeType("application/json")
+  rawFile.open("GET",file,true);
+  rawFile.onreadystatechange=function() {
+  if(rawFile.readyState===4 && rawFile.status=="200") {
+      callback(rawFile.responseText);
+
+  }
+  }
+
+  rawFile.send();
+
+}
+
+loadJSON("resume.json",function(text){
+
+  var data =JSON.parse(text);
+  // console.log(data);
+
+  basic(data.basics);
+});
+
+var main =document.getElementById('mainDiv');
+
+var left =document.getElementById('leftDiv');
+
+var right =document.getElementById('rightDiv');
+
+function basic(basics) {
+  var image =document.createElement("img");
+    image.classList.add("proimg")
+    image.src =basics.picture;
+
+      var lc1 =document.createElement("div");
+        lc1.classList.add("child1");
+    left.appendChild(image);
+  var head1=document.createElement("h1");
+    head1.textContent =basics.name;
+  var head2=document.createElement("h3");
+    head2.textContent =basics.label;
+  var hr1=document.createElement("hr");
+  var head3=document.createElement("h4");
+  head3.textContent =basics.email;
+  var head4=document.createElement("h4");
+  head4.textContent =basics.phone;
+
+lc1.appendChild(head1);
+lc1.appendChild(head2);
+lc1.appendChild(hr1);
+lc1.appendChild(head3);
+lc1.appendChild(head4);
+left.appendChild(lc1);
+
+
+
+
+}
---
+++ script.js
@@ -0,0 +1,17 @@
+// document.getElementsById('data')
+
+function newpage(){
+  window.open('resume1.html','_self')
+
+}
+
+function back(){
+
+window.open('index.html','_self')
+
+}
+
+function newpage1(){
+  window.open('sample.html','_self')
+
+}
